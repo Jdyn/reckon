11bf9669d84e7ac2979aaeb107a3863beff54fff
Jdyn <moorejaden5389@gmail.com>
2023-08-19 01:36:39 -0700

    update dependencies

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,34 +1,15 @@
-import { relativeAliasResolver } from '@reckon/config/vite';
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
-import svg from 'vite-plugin-svgr';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { mergeConfig } from 'vite';
+import baseConfig from '../../packages/config/vite';
 
-import { name, version } from './package.json';
-
-// https://vitejs.dev/config/
-export default defineConfig({
+export default mergeConfig(baseConfig, {
 	server: {
 		port: 3000
 	},
 	plugins: [
-		tsconfigPaths(),
-		react(),
-		svg({ svgrOptions: { icon: true } }),
-		createHtmlPlugin({
-			minify: true
+		visualizer({
+			gzipSize: true,
+			brotliSize: true
 		})
-	],
-	root: 'src',
-	define: {
-		pkgJson: { name, version }
-	},
-	resolve: {
-		alias: [relativeAliasResolver]
-	},
-	build: {
-		outDir: '../dist',
-		assetsDir: '.'
-	}
+	]
 });
