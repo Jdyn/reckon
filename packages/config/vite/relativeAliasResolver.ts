d58f400f4429d5bf1886c62e8399e9f2fd82495c
Jaden <moorejaden5389@gmail.com>
2023-08-19 18:51:50 -0700

    move client to root

--- packages/config/vite/relativeAliasResolver.ts
+++ packages/config/vite/relativeAliasResolver.ts
@@ -2,22 +2,68 @@ import fs from 'fs/promises';
 import path from 'path';
 import { Alias } from 'vite';
 
+const projectPath = path.resolve(__dirname, '../../../');
+const pkgJsonCache = new Map();
+
 const resolver: Alias = {
 	find: /^(~\/.+)/,
 	replacement: '$1',
 	async customResolver(source, importer) {
-		const [repo, filePath] = importer!.split('/packages/');
-		const [pkg] = filePath.split('/src/');
+		let root: null | string = null;
 
-		const sourcePath = source.substring(2);
+		const [_, sourcePath] = source.split('~/');
 
-		const absolutePath = `${repo}/packages/${pkg}/src/${sourcePath}`;
+		const relativeImporter = importer!.replace(projectPath, '');
+
+		if (relativeImporter.includes('/src/')) {
+			const [pkg] = relativeImporter.split('/src/');
+
+			root = `${projectPath}${pkg}/src`;
+		} else {
+			let parent = importer!;
+
+			while (parent !== '/') {
+				parent = path.dirname(parent);
+
+				let hasPkgJson = pkgJsonCache.get(parent);
+
+				if (hasPkgJson === undefined)
+					try {
+						await fs.stat(`${parent}/package.json`);
+						pkgJsonCache.set(parent, (hasPkgJson = true));
+					} catch {
+						pkgJsonCache.set(parent, (hasPkgJson = false));
+					}
+
+				if (hasPkgJson) {
+					root = parent;
+					break;
+				}
+			}
+
+			if (root === null)
+				throw new Error(`Failed to resolve import path ${source} in file ${importer}`);
+		}
+
+		const absolutePath = `${root}/${sourcePath}`;
 
 		const folderItems = await fs.readdir(path.join(absolutePath, '../'));
 
-		const item = folderItems.find((i) => i.startsWith(sourcePath.split('/').at(-1) ?? ''));
+		const item = folderItems.find((i) => i.startsWith(sourcePath.split('/').at(-1)!))!;
 
-		return absolutePath + path.extname(item!);
+		const fullPath = absolutePath + path.extname(item);
+
+		const stats = await fs.stat(fullPath);
+
+		if (stats.isDirectory()) {
+			const directoryItems = await fs.readdir(absolutePath + path.extname(item));
+
+			const indexFile = directoryItems.find((i) => i.startsWith('index'));
+
+			return `${absolutePath}/${indexFile}`;
+		} else {
+			return fullPath;
+		}
 	}
 };
 
