32b577b34e73144f1f17a5748c171758a3635a10
Jaden <moorejaden5389@gmail.com>
2023-10-01 11:43:34 -0700

    fix dialog states

--- client/src/app/Layout/GroupMenu.tsx
+++ client/src/app/Layout/GroupMenu.tsx
@@ -1,17 +1,13 @@
 import {
 	EllipsisHorizontalIcon,
-	EllipsisVerticalIcon,
-	ExclamationCircleIcon,
 	MagnifyingGlassIcon,
 	NewspaperIcon,
 	PlusCircleIcon,
 	UserPlusIcon
 } from '@heroicons/react/24/outline';
-import * as Portal from '@radix-ui/react-portal';
 import {
 	AlertDialog,
 	Button,
-	Callout,
 	Dialog,
 	DropdownMenu,
 	Flex,
@@ -30,16 +26,21 @@ const GroupMenu = () => {
 	const match = useMatch({ path: '/g/:id', caseSensitive: false, end: false });
 	const { data: group } = useGetGroupQuery(match?.params.id, { skip: !match });
 	const { data: user } = useAccountQuery();
-	const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState({
+		invite: false,
+		delete: false
+	});
 
 	const isCreator = useMemo(
 		() => group?.creator.id === user?.id || false,
 		[group?.creator.id, user?.id]
 	);
 
-	const handleDelete = () => {
-		
-	}
+	const handleDelete = () => {};
+
+	const handleDialog = (key: string, state: boolean) => {
+		setOpen((prev) => ({ ...prev, [key]: state }));
+	};
 
 	return match ? (
 		<SideMenuList>
@@ -47,18 +48,66 @@ const GroupMenu = () => {
 				{group?.name}
 				<DropdownMenu.Root>
 					<DropdownMenu.Trigger>
-						<IconButton variant="soft">
+						<IconButton variant="ghost">
 							<EllipsisHorizontalIcon width="18px" />
 						</IconButton>
 					</DropdownMenu.Trigger>
+
 					<DropdownMenu.Content style={{ width: '216px' }}>
-						<AlertDialog.Root open={open} onOpenChange={(open) => setOpen(open)}>
+						<Dialog.Root
+							open={open['invite']}
+							onOpenChange={(open) => handleDialog('invite', open)}
+						>
+							<Dialog.Trigger>
+								<DropdownMenu.Item
+									onClick={(e) => {
+										e.preventDefault();
+										handleDialog('invite', true);
+									}}
+								>
+									Invite friends
+								</DropdownMenu.Item>
+							</Dialog.Trigger>
+							<Dialog.Content style={{ maxWidth: 450 }}>
+								<Dialog.Title>Invite friends to {group?.name}</Dialog.Title>
+								<Flex direction="column" asChild>
+									<label>
+										<Text as="div" size="2" mb="1" weight="bold">
+											Identifier
+										</Text>
+										<TextField.Root>
+											<TextField.Slot>
+												<MagnifyingGlassIcon width="18px" />
+											</TextField.Slot>
+											<TextField.Input defaultValue="" placeholder="Phone, Email, Username" />
+											<TextField.Slot>
+												<Tooltip content="Send invite!">
+													<IconButton size="1" variant="soft">
+														<UserPlusIcon width="18px" />
+													</IconButton>
+												</Tooltip>
+											</TextField.Slot>
+										</TextField.Root>
+									</label>
+								</Flex>
+								<Flex gap="3" mt="4" justify="end">
+									<Dialog.Close>
+										<Button variant="soft">Done</Button>
+									</Dialog.Close>
+								</Flex>
+							</Dialog.Content>
+						</Dialog.Root>
+
+						<AlertDialog.Root
+							open={open['delete']}
+							onOpenChange={(open) => handleDialog('delete', open)}
+						>
 							<AlertDialog.Trigger>
 								<DropdownMenu.Item
 									color="red"
 									onClick={(e) => {
 										e.preventDefault();
-										setOpen(true);
+										handleDialog('delete', true);
 									}}
 								>
 									{isCreator ? 'Delete group' : 'Leave group'}
@@ -83,46 +132,13 @@ const GroupMenu = () => {
 						</AlertDialog.Root>
 					</DropdownMenu.Content>
 				</DropdownMenu.Root>
-				{/* <Dialog.Root>
-					<Dialog.Trigger>
-						<IconButton variant="soft" highContrast>
-							<UserPlusIcon width="18px" />
-						</IconButton>
-					</Dialog.Trigger>
-					<Dialog.Content style={{ maxWidth: 450 }}>
-						<Dialog.Title>Invite friends to {group?.name}</Dialog.Title>
-						<Flex direction="column">
-							<label>
-								<Text as="div" size="2" mb="1" weight="bold">
-									Identifier
-								</Text>
-								<TextField.Root>
-									<TextField.Slot>
-										<MagnifyingGlassIcon width="18px" />
-									</TextField.Slot>
-									<TextField.Input defaultValue="" placeholder="Phone, Email, Username" />
-									<TextField.Slot>
-										<Tooltip content="Send invite!">
-											<IconButton size="1" variant="soft">
-												<UserPlusIcon width="18px" />
-											</IconButton>
-										</Tooltip>
-									</TextField.Slot>
-								</TextField.Root>
-							</label>
-						</Flex>
-						<Flex gap="3" mt="4" justify="end">
-							<Dialog.Close>
-								<Button variant="soft">Done</Button>
-							</Dialog.Close>
-						</Flex>
-					</Dialog.Content>
-				</Dialog.Root> */}
 			</>
+
 			<SideMenuList.Link to={`${match.pathname}/feed`}>
 				<NewspaperIcon width="18px" />
 				Feed
 			</SideMenuList.Link>
+
 			<SideMenuList.Link to={`${match.pathname}/new`}>
 				<PlusCircleIcon width="18px" />
 				New
