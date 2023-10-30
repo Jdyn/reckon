174edc885ccb92c4a9b342788e13f3110f7d731d
Jaden <moorejaden5389@gmail.com>
2023-10-29 22:27:51 -0700

    clean up bill list code

--- server/web/context/bills/bills.ex
+++ server/web/context/bills/bills.ex
@@ -6,65 +6,51 @@ defmodule Nimble.Bills do
   alias Nimble.BillCharge
   alias Nimble.Bills.Query
   alias Nimble.Repo
+  alias Nimble.User
   alias Nimble.UserLike
 
-  def for_global(user_id) do
-    bills =
-      Bill
-      |> order_by([b], desc: b.inserted_at)
-      |> join(:left, [b], ul in UserLike, on: ul.bill_id == b.id and ul.user_id == ^user_id)
-      |> limit(10)
-      |> select([b, ul], %{b | liked: not is_nil(ul.id)})
-      |> dbg
-      |> Repo.all()
-      |> Repo.preload([:creator])
-
-    {:ok, bills}
+  def index(%{user_id: user_id}, :global) do
+    {:ok,
+     Bill
+     |> join_liked(user_id)
+     |> order_by([b], desc: b.inserted_at)
+     |> limit(10)
+     |> Repo.all()
+     |> Repo.preload([:creator])}
   end
 
-  def for_group(group_id, user_id) do
-    bills =
-      from(b in Bill, where: b.group_id == ^group_id, order_by: [desc: b.inserted_at])
-      |> join(:left, [b], ul in UserLike, on: ul.bill_id == b.id and ul.user_id == ^user_id)
-      |> select([b, ul], %{b | liked: not is_nil(ul.id)})
-      |> Repo.all()
-      |> Repo.preload([:items, :creator, charges: [:user]])
-
-    {:ok, bills}
+  def index(%{group_id: group_id, user_id: user_id}, :group) do
+    {:ok,
+     [group_id: group_id]
+     |> Query.bill()
+     |> join_liked(user_id)
+     |> order_by([b], desc: b.inserted_at)
+     |> Repo.all()
+     |> Repo.preload([:items, :group, :creator, charges: [:user]])}
   end
 
-  def for_user(user) do
-    bills =
-      user
-      |> assoc(:associated_bills)
-      |> order_by([b], desc: b.inserted_at)
-      |> join(:left, [b], ul in UserLike, on: ul.bill_id == b.id and ul.user_id == ^user.id)
-      |> select([b, bc, ul], %{b | liked: not is_nil(ul.id)})
-      |> Repo.all()
-      |> Repo.preload([:items, :group, :creator, charges: [:user]])
-
-    {:ok, bills}
+  def index(%{user: %User{} = user}, :user) do
+    {:ok,
+     user
+     |> assoc(:associated_bills)
+     |> join_liked(user.id)
+     |> order_by([b], desc: b.inserted_at)
+     |> Repo.all()
+     |> Repo.preload([:items, :group, :creator, charges: [:user]])}
   end
 
-  def create(group_id, creator_id, params) do
-    attrs = Map.merge(params, %{"creator_id" => creator_id, "group_id" => group_id})
-
-    %Bill{}
-    |> Bill.create_changeset(attrs)
-    |> Repo.insert()
-    |> case do
-      {:ok, bill} ->
-        {:ok, Repo.preload(bill, [:items, :creator, charges: [:user]])}
-
-      error ->
-        error
+  def create(params) do
+    with {:ok, bill} <-
+           %Bill{}
+           |> Bill.create_changeset(params)
+           |> Repo.insert() do
+      {:ok, Repo.preload(bill, [:items, :creator, charges: [:user]])}
     end
   end
 
-  def get_bill(bill_id) do
-    Bill
-    |> Repo.get(bill_id)
-    |> Repo.preload([:items, :group, :creator, charges: [:user]])
+  def get_bill(id) do
+    bill = Repo.get(Bill, id)
+    Repo.preload(bill, [:items, :group, :creator, charges: [:user]])
   end
 
   def update_charge(charge_id, user_id, params) do
@@ -77,27 +63,33 @@ defmodule Nimble.Bills do
     end
   end
 
-  def like(bill_id, user_id) do
-    case Repo.one(from(l in UserLike, where: l.bill_id == ^bill_id, where: l.user_id == ^user_id)) do
+  def like(id, user_id) do
+    case Repo.one(Query.bill_like(id, user_id)) do
       nil ->
         %UserLike{}
-        |> UserLike.changeset(%{"bill_id" => bill_id, "user_id" => user_id})
-        |> Repo.insert()
+        |> UserLike.changeset(%{"id" => id, "user_id" => user_id})
+        |> Repo.insert!()
 
-        inc_likes(bill_id, 1)
+        inc_likes(id, 1)
 
       like ->
-        Repo.delete(like)
-        inc_likes(bill_id, -1)
+        Repo.delete!(like)
+        inc_likes(id, -1)
     end
   end
 
-  def liked?(bill_id, user_id) do
-    Repo.exists?(from(l in UserLike, where: l.bill_id == ^bill_id, where: l.user_id == ^user_id))
+  def liked?(id, user_id) do
+    Repo.exists?(Query.bill_like(id, user_id))
   end
 
-  def inc_likes(bill_id, value) do
-    Repo.update_all(from(b in Bill, where: b.id == ^bill_id, update: [inc: [like_count: ^value]]), [])
+  def join_liked(query, user_id) do
+    query
+    |> join(:left, [b], ul in UserLike, on: ul.id == b.id and ul.user_id == ^user_id)
+    |> select([b, ul], %{b | liked: not is_nil(ul.id)})
+  end
+
+  def inc_likes(id, value) do
+    Repo.update_all(from(b in Bill, where: b.id == ^id, update: [inc: [like_count: ^value]]), [])
     :ok
   end
 end
--- server/web/controllers/bill_controller.ex
+++ server/web/controllers/bill_controller.ex
@@ -1,14 +1,15 @@
 defmodule Nimble.BillController do
   use Nimble.Web, :controller
 
-  alias Nimble.Bills
   alias Nimble.Bill
+  alias Nimble.Bills
 
   action_fallback(Nimble.ErrorController)
 
   def global_bills(conn, _params) do
     user = current_user(conn)
-    with {:ok, bills} <- Bills.for_global(user.id) do
+
+    with {:ok, bills} <- Bills.index(%{user_id: user.id}, :global) do
       render(conn, "external_index.json", bills: bills)
     end
   end
@@ -16,7 +17,8 @@ defmodule Nimble.BillController do
   def group_bills(conn, _params) do
     group_id = conn.assigns[:group_id]
     user = current_user(conn)
-    with {:ok, bills} <- Bills.for_group(group_id, user.id) do
+
+    with {:ok, bills} <- Bills.index(%{group_id: group_id, user_id: user.id}, :group) do
       render(conn, "index.json", bills: bills)
     end
   end
@@ -24,21 +26,24 @@ defmodule Nimble.BillController do
   def user_bills(conn, _params) do
     user = current_user(conn)
 
-    with {:ok, bills} <- Bills.for_user(user) do
+    with {:ok, bills} <- Bills.index(%{user: user}, :user) do
       render(conn, "user_index.json", bills: bills)
     end
   end
 
-  def show(conn, %{ "id" => bill_id}) do
+  def show(conn, %{"id" => bill_id}) do
     with bill = %Bill{} <- Bills.get_bill(bill_id) do
       render(conn, "show.json", bill: bill)
     end
   end
 
   def create(conn, params) do
-    %{group_id: group_id, current_user: current_user} = conn.assigns
+    %{group_id: group_id } = conn.assigns
+    user = current_user(conn)
 
-    with {:ok, bill} <- Bills.create(group_id, current_user.id, params) do
+    params = Map.merge(params, %{"creator_id" => user.id, "group_id" => group_id})
+
+    with {:ok, bill} <- Bills.create(params) do
       render(conn, "show.json", bill: bill)
     end
   end
@@ -51,11 +56,10 @@ defmodule Nimble.BillController do
     end
   end
 
-  def like(conn, %{ "bill_id" => bill_id}) do
+  def like(conn, %{"bill_id" => bill_id}) do
     user = current_user(conn)
 
     Bills.like(bill_id, user.id)
-    json(conn, %{ ok: true })
-
+    json(conn, %{ok: true})
   end
 end
